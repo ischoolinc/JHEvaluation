1d5bbf0cbc5e8313956557d4c1b91caadd7dee51
chentaichen <ct.chen@ischool.com.tw>
2014-04-28 16:22:54 +0800

    調整評量比例預設值。

--- JHHsinChuScoreReports/HsinChuExamScore_JH/HsinChuExamScore_JH/Utility.cs
+++ JHHsinChuScoreReports/HsinChuExamScore_JH/HsinChuExamScore_JH/Utility.cs
@@ -293,9 +293,11 @@ namespace HsinChuExamScore_JH
             {
                 string id = dr["id"].ToString();
                 decimal sp = 50;
-                decimal.TryParse(dr["ScorePercentage"].ToString(), out sp);
-
-                returnData.Add(id, sp);
+                if(decimal.TryParse(dr["ScorePercentage"].ToString(), out sp))
+                    returnData.Add(id, sp);
+                else
+                    returnData.Add(id, 50);
+                
             }
             return returnData;
         }
