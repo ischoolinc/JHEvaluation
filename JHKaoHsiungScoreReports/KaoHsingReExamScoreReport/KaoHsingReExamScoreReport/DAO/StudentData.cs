460ac04d63e20d9026dc931be28770f010b7e727
chentaichen <ct.chen@ischool.com.tw>
2015-06-15 18:14:39 +0800

    高雄補考報表建立。

---
+++ JHKaoHsiungScoreReports/KaoHsingReExamScoreReport/KaoHsingReExamScoreReport/DAO/StudentData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JHSchool.Data;
+
+namespace KaoHsingReExamScoreReport.DAO
+{
+    public class StudentData
+    {
+        public string StudentID { get; set; }
+
+        /// <summary>
+        /// 學生姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 班級名稱
+        /// </summary>
+        public string ClassName { get; set; }
+
+        /// <summary>
+        /// 學號
+        /// </summary>
+        public string StudentNumber { get; set; }
+
+        /// <summary>
+        /// 年級
+        /// </summary>
+        public string GradeYear { get; set; }
+
+        /// <summary>
+        /// 座號
+        /// </summary>
+        public string SeatNo { get; set; }
+
+        public string ClassID { get; set; }
+        /// <summary>
+        /// 學期成績
+        /// </summary>
+        public JHSemesterScoreRecord SemesterScoreRecord = new JHSemesterScoreRecord();
+    }
+}
---
+++ JHKaoHsiungScoreReports/KaoHsingReExamScoreReport/KaoHsingReExamScoreReport/Forms/ReDomainForTeacherForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FISCA.Presentation.Controls;
+using KaoHsingReExamScoreReport.DAO;
+
+namespace KaoHsingReExamScoreReport.Forms
+{
+    public partial class ReDomainForTeacherForm : BaseForm
+    {
+        BackgroundWorker _bgWorker;
+        int _SchoolYear = 0;
+        int _Semester = 0;
+        List<ClassData> _ClassDataList;
+        List<StudentData> _StudentDataList;
+        List<string> _SelectClassIDList;
+
+        public ReDomainForTeacherForm(List<string> ClassIDList)
+        {
+            InitializeComponent();
+            _bgWorker = new BackgroundWorker();
+            _ClassDataList = new List<ClassData>();
+            _StudentDataList = new List<StudentData>();
+            _SelectClassIDList = ClassIDList;
+            _bgWorker.DoWork += _bgWorker_DoWork;
+            _bgWorker.RunWorkerCompleted += _bgWorker_RunWorkerCompleted;
+            _bgWorker.ProgressChanged += _bgWorker_ProgressChanged;
+            _bgWorker.WorkerReportsProgress = true;
+        }
+
+        void _bgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            FISCA.Presentation.MotherForm.SetStatusBarMessage("補考名單產生中 ...", e.ProgressPercentage);
+        }
+
+        void _bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnPrint.Enabled = true;
+        }
+
+        void _bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ReDomainForTeacherForm_Load(object sender, EventArgs e)
+        {
+            this.MinimumSize = this.MaximumSize = this.Size;
+
+            // 預設學年度、學期
+            iptSchoolYear.Value = int.Parse(K12.Data.School.DefaultSchoolYear);
+            iptSemester.Value = int.Parse(K12.Data.School.DefaultSemester);
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            
+            // 使用者選學年度、學期
+            _SchoolYear = iptSchoolYear.Value;
+            _Semester = iptSemester.Value;
+
+            _bgWorker.RunWorkerAsync();
+        }
+    }
+}
