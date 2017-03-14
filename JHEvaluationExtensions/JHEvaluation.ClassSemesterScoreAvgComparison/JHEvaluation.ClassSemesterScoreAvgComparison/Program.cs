e1175b6f2872e76f8b20cf0cd0eeb38cac35a256
ED <yinghua.lin@ischool.com.tw>
2017-03-14 16:46:39 +0800

    2017/3/14 穎驊修正  班級學期成績平均比較 功能的 Library

--- JHEvaluationExtensions/JHEvaluation.ClassSemesterScoreAvgComparison/JHEvaluation.ClassSemesterScoreAvgComparison/Program.cs
+++ JHEvaluationExtensions/JHEvaluation.ClassSemesterScoreAvgComparison/JHEvaluation.ClassSemesterScoreAvgComparison/Program.cs
@@ -4,20 +4,20 @@ using System.Windows.Forms;
 using JHSchool;
 using Framework;
 using Framework.Security;
-using FISCA;
-using FISCA.Presentation;
+
+
 
 namespace JHEvaluation.ClassSemesterScoreAvgComparison
 {
     public static class Program
     {
-        [MainMethod]
+        [FISCA.MainMethod]
         public static void Main()
         {
             Catalog detail = RoleAclSource.Instance["班級"]["報表"];
             detail.Add(new ReportFeature("KaoHsiung.JHEvaluation.Class.ClassSemesterScoreAvgComparison", "班級學期成績比較表"));
 
-            MenuButton mb = FISCA.Presentation.MotherForm.RibbonBarItems["班級", "資料統計"]["報表"]["成績相關報表"]["班級學期成績平均比較表"];
+            FISCA.Presentation.MenuButton mb = FISCA.Presentation.MotherForm.RibbonBarItems["班級", "資料統計"]["報表"]["成績相關報表"]["班級學期成績平均比較表"];
             mb.Enable = false;
             mb.Click += delegate
             {
