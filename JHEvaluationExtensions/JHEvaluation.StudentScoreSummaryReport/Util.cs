950a3acb5d2c87e537ca3111c0809bbde85d13b2
ED <yinghua.lin@ischool.com.tw>
2018-08-25 21:00:28 +0800

    2018/08/25 穎驊修正 康橋新竹國中 在校成績證明單(英文版)的等第問題，注意本次的更新將會影響全國中系統，需要找一天改回來。

--- JHEvaluationExtensions/JHEvaluation.StudentScoreSummaryReport/Util.cs
+++ JHEvaluationExtensions/JHEvaluation.StudentScoreSummaryReport/Util.cs
@@ -339,11 +339,11 @@ namespace JHEvaluation.StudentScoreSummaryReport
 
         public static string GetDegreeEnglish(decimal score)
         {
-            if (score >= 90) return "A";
-            else if (score >= 80) return "B";
-            else if (score >= 70) return "C";
-            else if (score >= 60) return "D";
-            else return "E";
+            if (score >= 90) return "A+";
+            else if (score >= 80) return "A";
+            else if (score >= 70) return "B";
+            else if (score >= 60) return "C";
+            else return "D";
         }
 
         /// <summary>
