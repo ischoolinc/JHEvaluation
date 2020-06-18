42f28704a8eef9e822737c2bc38066f18554c97e
3DB2\dylan <dylan.huang@ischool.com.tw>
2020-06-18 16:25:23 +0800

    調整Checkbox位置

--- JHEvaluationExtensions/JHEvaluationAssignmentScore/CourseScoreStatusForm.Designer.cs
+++ JHEvaluationExtensions/JHEvaluationAssignmentScore/CourseScoreStatusForm.Designer.cs
@@ -59,7 +59,7 @@
             // 
             this.intSemester.BackgroundStyle.Class = "DateTimeInputBackground";
             this.intSemester.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.intSemester.Location = new System.Drawing.Point(248, 8);
+            this.intSemester.Location = new System.Drawing.Point(248, 20);
             this.intSemester.MaxValue = 2;
             this.intSemester.MinValue = 1;
             this.intSemester.Name = "intSemester";
@@ -76,7 +76,7 @@
             // 
             this.intSchoolYear.BackgroundStyle.Class = "DateTimeInputBackground";
             this.intSchoolYear.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.intSchoolYear.Location = new System.Drawing.Point(86, 8);
+            this.intSchoolYear.Location = new System.Drawing.Point(86, 20);
             this.intSchoolYear.MaxValue = 150;
             this.intSchoolYear.MinValue = 50;
             this.intSchoolYear.Name = "intSchoolYear";
@@ -93,7 +93,7 @@
             // 
             this.labelX2.BackgroundStyle.Class = "";
             this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.labelX2.Location = new System.Drawing.Point(195, 9);
+            this.labelX2.Location = new System.Drawing.Point(195, 21);
             this.labelX2.Name = "labelX2";
             this.labelX2.Size = new System.Drawing.Size(46, 23);
             this.labelX2.TabIndex = 2;
@@ -107,7 +107,7 @@
             // 
             this.labelX1.BackgroundStyle.Class = "";
             this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.labelX1.Location = new System.Drawing.Point(8, 9);
+            this.labelX1.Location = new System.Drawing.Point(8, 21);
             this.labelX1.Name = "labelX1";
             this.labelX1.Size = new System.Drawing.Size(75, 23);
             this.labelX1.TabIndex = 3;
@@ -131,9 +131,9 @@
             this.chEffort,
             this.chText});
             this.listView.FullRowSelect = true;
-            this.listView.Location = new System.Drawing.Point(8, 39);
+            this.listView.Location = new System.Drawing.Point(8, 65);
             this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(645, 456);
+            this.listView.Size = new System.Drawing.Size(645, 430);
             this.listView.TabIndex = 4;
             this.listView.UseCompatibleStateImageBehavior = false;
             this.listView.View = System.Windows.Forms.View.Details;
@@ -184,7 +184,7 @@
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(90, 25);
             this.btnRefresh.TabIndex = 6;
-            this.btnRefresh.Text = "重新整理";            
+            this.btnRefresh.Text = "重新整理";
             // 
             // btnClose
             // 
@@ -220,7 +220,7 @@
             this.chkDisplayNotFinish.AutoSize = true;
             this.chkDisplayNotFinish.BackColor = System.Drawing.Color.Transparent;
             this.chkDisplayNotFinish.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(66)))), ((int)(((byte)(139)))));
-            this.chkDisplayNotFinish.Location = new System.Drawing.Point(479, 10);
+            this.chkDisplayNotFinish.Location = new System.Drawing.Point(360, 38);
             this.chkDisplayNotFinish.Name = "chkDisplayNotFinish";
             this.chkDisplayNotFinish.Size = new System.Drawing.Size(170, 21);
             this.chkDisplayNotFinish.TabIndex = 3;
@@ -237,7 +237,7 @@
             // 
             this.lblCourseCount.BackgroundStyle.Class = "";
             this.lblCourseCount.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.lblCourseCount.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblCourseCount.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lblCourseCount.ForeColor = System.Drawing.Color.Red;
             this.lblCourseCount.Location = new System.Drawing.Point(166, 504);
             this.lblCourseCount.Name = "lblCourseCount";
@@ -254,7 +254,7 @@
             // 
             this.lblMsg.BackgroundStyle.Class = "";
             this.lblMsg.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.lblMsg.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblMsg.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lblMsg.ForeColor = System.Drawing.Color.Red;
             this.lblMsg.Location = new System.Drawing.Point(360, 503);
             this.lblMsg.Name = "lblMsg";
@@ -268,7 +268,7 @@
             this.chkText.AutoSize = true;
             this.chkText.BackColor = System.Drawing.Color.Transparent;
             this.chkText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(66)))), ((int)(((byte)(139)))));
-            this.chkText.Location = new System.Drawing.Point(360, 10);
+            this.chkText.Location = new System.Drawing.Point(360, 9);
             this.chkText.Name = "chkText";
             this.chkText.Size = new System.Drawing.Size(105, 21);
             this.chkText.TabIndex = 2;
