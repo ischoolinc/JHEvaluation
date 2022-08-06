6677992255dee96fec83554deecdd4a7cfc2a492
Cynthia Chen <74887146+cynthiaischool@users.noreply.github.com>
2022-08-06 20:38:43 +0800

    領域資料管理，將本土語文加入108課綱領域判斷(顯示勾選)

--- JHSchool.Evaluation/Global.cs
+++ JHSchool.Evaluation/Global.cs
@@ -12,7 +12,8 @@ namespace JHSchool.Evaluation
             DomainMap108List.Clear();
 
             // 加入國語文、英語 主要是因為高雄版在資料庫內沒有語文領域，讓使用者可以明確資料這2個領域也會被算入
-            DomainMap108List = new List<string>(new string[] { "語文", "數學", "自然科學", "科技", "社會", "藝術", "健康與體育", "綜合活動", "藝術與人文", "自然與生活科技", "國語文", "英語" });
+            //2022/08 高雄加入本土語文
+            DomainMap108List = new List<string>(new string[] { "語文", "數學", "自然科學", "科技", "社會", "藝術", "健康與體育", "綜合活動", "藝術與人文", "自然與生活科技", "國語文", "英語", "本土語文" });
         }
 
         /// <summary>
